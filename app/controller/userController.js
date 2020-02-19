d163208c2a3195d8344a6d5aeefc08d7fd7045d4
phursang <psherpa592@gmail.com>
2020-02-19 16:53:50 +0530

    updated package with pino and moment,added loggerLib and checkLib

--- app/controller/userController.js
+++ app/controller/userController.js
@@ -1,11 +1,37 @@
 /**
  * Controller  for routes
  */
+let checkLib = require('../lib/checkLib');
 
 //Signing up user
 let signUpUser = (req,res)=>
 {
-    res.send('signup');
+     //validating email
+     let validateUserInput = () => {
+        return new Promise((resolve, reject) => {
+            if (req.body.email) {
+                if (!validateInput.Email(req.body.email)) {
+                    apiResponse = response.generate(true, 'email does not met the requirement', 400, null);
+                    logger.error('not valid email', 'userController:validateUserInput', 10)
+                    reject(apiResponse)
+                }
+                else if (checkLib.isEmpty(req.body.password)) {
+                    apiResponse = response.generate(true, 'password is empty', 400, null)
+                    logger.error('password is empty', 'userController:validateUserInput', 10)
+                    reject(apiResponse)
+                }
+                else {
+                    logger.info('user validated', 'userController:validateUserInput', 10);
+                    resolve(req);
+                }
+            }
+            else {
+                logger.error('email field missing during user creation', 'userController:signUpFunction', 10);
+                apiResponse = response.generate(true, 'one or more parameter is missing', 400, null);
+                reject(apiResponse);
+            }
+        });
+    }//end of validate user input
 }
 //logging in user
 let logInUser = (req,res)=>
