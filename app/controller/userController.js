6af2d493957854856c23896952707c4a411944d9
phursang <psherpa592@gmail.com>
2020-02-20 11:40:39 +0530

    added timeLib and generatePasswordLib

--- app/controller/userController.js
+++ app/controller/userController.js
@@ -5,6 +5,10 @@ let checkLib = require('../lib/checkLib');
 let validateInput = require('../lib/paramsValidationLib')
 let response = require('../lib/responseLib')
 let logger = require('../lib/loggerLib')
+let shortid = require('shortid');
+
+let mongoose = require('mongoose')
+let UserModel = mongoose.model('User');
 
 //Signing up user
 let signUpUser = (req, res) => {
@@ -37,16 +41,76 @@ let signUpUser = (req, res) => {
         });
     }//end of validate user input
 
+    //creating user after input validation
+    let createUser = () => {
+        return new Promise((resolve, reject) => {
+            UserModel.findOne({ email: req.body.email })
+                .exec((err, retrievedUserDetails) => {
+                    if (err) {
+                        logger.error(err.message, 'userController: createUser', 10)
+                        let apiResponse = response.generate(true, 'Failed To Create User', 500, null)
+                        reject(apiResponse)
+                    } else if (check.isEmpty(retrievedUserDetails)) {
+                        console.log(req.body)
+                        let newUser = new UserModel({
+                            userId: shortid.generate(),
+                            firstName: req.body.firstName,
+                            lastName: req.body.lastName || '',
+                            userName: req.body.userName,
+                            isAdmin: req.body.isAdmin,
+                            countryName: req.body.countryName,
+                            mobileNumber: req.body.mobileNumber,
+                            email: req.body.email.toLowerCase(),
+                            password: passwordLib.hashpassword(req.body.password),
+                            createdOn: time.now()
+                        })
+                        newUser.save((err, newUser) => {
+                            if (err) {
+                                console.log(err)
+                                logger.error(err.message, 'userController: createUser', 10)
+                                let apiResponse = response.generate(true, 'Failed to create new User', 500, null)
+                                reject(apiResponse)
+                            } else {
+                                let newUserObj = newUser.toObject();
+                                console.log(`${applicationUrl}/verify-email/${newUserObj.userId}`)
+                                //Creating object for sending welcome email
+                                let sendEmailOptions = {
+                                    email: newUserObj.email,
+                                    name: newUserObj.firstName + ' ' + newUserObj.lastName,
+                                    subject: 'Welcome to Lets Meet ',
+                                    html: `<b> Dear ${newUserObj.firstName}</b><br> Hope you are doing well. 
+                                    <br>Welcome to our Meeting Planner App <br>
+                                    Please click on following link to verify your account with Lets Meet.<br>
+                                    <br> <a href="${applicationUrl}/verify-email/${newUserObj.userId}">Click Here</a>                                     
+                                    `
+                                }
+
+                                setTimeout(() => {
+                                    emailLib.sendEmail(sendEmailOptions);
+                                }, 2000);
+
+                                resolve(newUserObj)
+                            }
+                        })
+                    } else {
+                        logger.error('User Cannot Be Created.User Already Present', 'userController: createUser', 4)
+                        let apiResponse = response.generate(true, 'User Already Present With this Email', 403, null)
+                        reject(apiResponse)
+                    }
+                })
+        })
+    }// end create user function
+
+
     validateUserInput(req, res)
         .then((resolve) => {
             console.log('inside resolve')
             apiResponse = response.generate(false, 'user created', 200, resolve);
             res.send(apiResponse);
         })
-        .catch((error)=>
-        {
+        .catch((error) => {
             console.log('inside catch')
-            apiResponse = response.generate(true,error.message,500,null);
+            apiResponse = response.generate(true, error.message, 500, null);
             res.send(apiResponse)
         })
 }
