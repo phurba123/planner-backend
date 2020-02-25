d3d28ced8718ca34bdafae783604ed063f94f638
phursang <psherpa592@gmail.com>
2020-02-25 20:49:17 +0530

    add meeting completed

--- app/controller/meetingController.js
+++ app/controller/meetingController.js
@@ -1,5 +1,12 @@
+let logger = require('../lib/loggerLib');
+const mongoose = require('mongoose');
+let meetingModel = mongoose.model('Meeting');
+let shortid = require('shortid');
+let time = require('../lib/timeLib');
+let response = require('../lib/responseLib')
+
 //adding meeting
-let addMeeting = () => {
+let addMeeting = (req,res) => {
     let validateUserInput = () => {
         return new Promise((resolve, reject) => {
             if (req.body.topic && req.body.hostId && req.body.hostName &&
@@ -13,15 +20,63 @@ let addMeeting = () => {
             }
         })
     }// end validate user input
+
+    let addMeetingAfterValidation = () => {
+        return new Promise((resolve, reject) => {
+            //console.log(req.body)
+            let newMeeting = new meetingModel({
+                meetingId: shortid.generate(),
+                topic: req.body.topic,
+                hostId: req.body.hostId,
+                hostName: req.body.hostName,
+                participantId: req.body.participantId,
+                participantName: req.body.participantName,
+                participantEmail: req.body.participantEmail,
+                meetingStartDate: req.body.meetingStartDate,
+                meetingEndDate: req.body.meetingEndDate,
+                meetingDescription: req.body.meetingDescription,
+                meetingPlace: req.body.meetingPlace,
+                createdOn: time.now()
+            })
+
+            console.log(newMeeting)
+            newMeeting.save((err, newMeeting) => {
+                if (err) {
+                    console.log(err)
+                    logger.error(err.message, 'meetingController: addMeeting', 10)
+                    let apiResponse = response.generate(true, 'Failed to add new Meeting', 500, null)
+                    reject(apiResponse)
+                }
+                else {
+                    let newMeetingObj = newMeeting.toObject();
+                    delete newMeetingObj._id;
+                    delete newMeetingObj.__v;
+                    resolve(newMeetingObj)
+                }
+            })
+
+        })
+    }// end addMeeting function
+
+    validateUserInput(req, res)
+        .then(addMeetingAfterValidation)
+        .then((resolve) => {
+            let apiResponse = response.generate(false, 'Meeting Created', 200, resolve)
+            res.send(apiResponse)
+        })
+        .catch((err) => {
+            console.log(err);
+            res.send(err);
+        })
 }//end of adding meeting
 
 //updating meeting
-let updateMeeting = () => {
+let updateMeeting = (req,res) => {
     //
 }//end of updating meeting
 
 //deleting meeting by id
-let deleteMeeting = () => {
+let deleteMeeting = (req,res) => {
     //
 }//end of deleting meeting
 
