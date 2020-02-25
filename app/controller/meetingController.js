4fc2051e6c26f9ece49aa4ecdc5c0628ba3a776b
phursang <psherpa592@gmail.com>
2020-02-25 21:18:34 +0530

    updating meeting completed

--- app/controller/meetingController.js
+++ app/controller/meetingController.js
@@ -3,10 +3,11 @@ const mongoose = require('mongoose');
 let meetingModel = mongoose.model('Meeting');
 let shortid = require('shortid');
 let time = require('../lib/timeLib');
-let response = require('../lib/responseLib')
+let response = require('../lib/responseLib');
+let check = require('../lib/checkLib')
 
 //adding meeting
-let addMeeting = (req,res) => {
+let addMeeting = (req, res) => {
     let validateUserInput = () => {
         return new Promise((resolve, reject) => {
             if (req.body.topic && req.body.hostId && req.body.hostName &&
@@ -21,6 +22,7 @@ let addMeeting = (req,res) => {
         })
     }// end validate user input
 
+    //adding meeting promise
     let addMeetingAfterValidation = () => {
         return new Promise((resolve, reject) => {
             //console.log(req.body)
@@ -55,7 +57,8 @@ let addMeeting = (req,res) => {
                 }
             })
 
-        })
+        })//adding meeting promise
+
     }// end addMeeting function
 
     validateUserInput(req, res)
@@ -71,12 +74,71 @@ let addMeeting = (req,res) => {
 }//end of adding meeting
 
 //updating meeting
-let updateMeeting = (req,res) => {
-    //
+let updateMeeting = (req, res) => {
+
+    //finding meeting with id
+    let findMeetingDetails = () => {
+        return new Promise((resolve, reject) => {
+            meetingModel.findOne({ 'meetingId': req.params.meetingId })
+                .select('-id -__v')
+                .lean()
+                .exec((err, meetingDetails) => {
+                    if (err) {
+                        console.log(err)
+                        logger.error(err.message, 'Meeting Controller: findMeetingDetails', 10)
+                        let apiResponse = response.generate(true, 'Failed To Find Meeting Details', 500, null)
+                        reject(apiResponse)
+                    } else if (check.isEmpty(meetingDetails)) {
+                        logger.info('No Meeting Found', 'Meeting  Controller:findMeetingDetails')
+                        let apiResponse = response.generate(true, 'No Meeting Found', 404, null)
+                        reject(apiResponse)
+                    } else {
+                        let apiResponse = response.generate(false, 'Meeting Details Found', 200, meetingDetails)
+                        resolve(meetingDetails)
+                    }
+                })
+        })
+    }// end findmeetingdetails
+
+    //updating meeting promise
+    let updateMeetingAfterFinding = (meetingDetails) => {
+        return new Promise((resolve, reject) => {
+
+            let options = req.body;
+            meetingModel.update({ 'meetingId': req.params.meetingId }, options).exec((err, result) => {
+                if (err) {
+                    console.log(err)
+                    logger.error(err.message, 'Meeting Controller:updateMeeting', 10)
+                    let apiResponse = response.generate(true, 'Failed To Update Meeting details', 500, null)
+                    reject(apiResponse)
+                } else if (check.isEmpty(result)) {
+                    logger.info('No Meeting Found', 'Meeting Controller:updateMeeting')
+                    let apiResponse = response.generate(true, 'No Meeting Found', 404, null)
+                    reject(apiResponse)
+                } else {
+                    let apiResponse = response.generate(false, 'Meeting details Updated', 200, result)
+                    resolve(result)
+                }
+            });// end meeting model update
+
+        })
+    }// end updateMeetingAfterFinding promise
+
+    findMeetingDetails(req, res)
+        .then(updateMeetingAfterFinding)
+        .then((resolve) => {
+            let apiResponse = response.generate(false, 'Meeting Updated', 200, resolve)
+            res.send(apiResponse)
+        })
+        .catch((err) => {
+            console.log(err);
+            res.send(err);
+        })
+
 }//end of updating meeting
 
 //deleting meeting by id
-let deleteMeeting = (req,res) => {
+let deleteMeeting = (req, res) => {
     //
 }//end of deleting meeting
 
